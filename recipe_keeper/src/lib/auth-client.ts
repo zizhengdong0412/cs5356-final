7d5c75182b44e4f634c65a5bbb26d56b8108cdbf
zizhengdong0412 <zizheng.dong0412@gmail.com>
2025-04-17 17:19:24 -0400

    Revert "Major Merged some elements from HW6"
    This reverts commit 1a0f2ced7599aa54bd64545cbcae921159484be8.

--- recipe_keeper/src/lib/auth-client.ts
+++
@@ -1,6 +0,0 @@
-import { createAuthClient } from "better-auth/react"
-import { adminClient, inferAdditionalFields } from "better-auth/client/plugins";
-import type { auth } from "./lib/auth" 
-export const authClient = createAuthClient({
-    plugins: [inferAdditionalFields<typeof auth>(),adminClient(),]
-})
