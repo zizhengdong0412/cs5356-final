4710ccea9e026c497da68952a428607819b5ed4c
zizhengdong0412 <zizheng.dong0412@gmail.com>
2025-04-17 15:12:03 -0400

    Restart

--- node_modules/@simplewebauthn/server/esm/helpers/convertCOSEtoPKCS.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Takes COSE-encoded public key and converts it to PKCS key
- */
-export declare function convertCOSEtoPKCS(cosePublicKey: Uint8Array): Uint8Array;
-//# sourceMappingURL=convertCOSEtoPKCS.d.ts.map
