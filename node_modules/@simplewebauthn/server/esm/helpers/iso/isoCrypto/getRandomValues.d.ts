3f48b383e5248a885ae12ec2fe40174694a5d851
huyunhao38786 <112986437+huyunhao38786@users.noreply.github.com>
2025-04-23 16:34:12 -0400

    Remove node_modules from git tracking

--- node_modules/@simplewebauthn/server/esm/helpers/iso/isoCrypto/getRandomValues.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Fill up the provided bytes array with random bytes equal to its length.
- *
- * @returns the same bytes array passed into the method
- */
-export declare function getRandomValues(array: Uint8Array): Promise<Uint8Array>;
-//# sourceMappingURL=getRandomValues.d.ts.map
